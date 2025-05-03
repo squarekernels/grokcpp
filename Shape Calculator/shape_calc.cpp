d98ee291e77be52a19124e93a2405db8eacc0bee
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-04 00:44:25 +0500

    display shape selection menu

--- Shape Calculator/shape_calc.cpp
+++ Shape Calculator/shape_calc.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void print_menu() {
+    std::vector<std::string> shapes = {
+        "Exit",
+        "Circle",
+        "Square",
+        "Triangle",
+        "Rectangle",
+        "Pentagon",
+        "Hexagon",
+        "Heptagon",
+        "Octagon",
+        "Nonagon",
+        "Decagon",
+    };
+
+    for (size_t i = 1; i < shapes.size(); ++i) {
+        std::cout << i << ". " << shapes[i] << std::endl;
+    }
+    std::cout << "0. " << shapes[0] << "\n\n" << std::endl;
+}
 
 int main() {
     std::cout<<"Welcome to the Geometry Calculator!\n\n"<<std::endl;
 
     std::cout<<"Choose a shape:\n"<<std::endl;
-    std::cout<<"1. Circle"<<std::endl;
-    std::cout<<"2. Triangle"<<std::endl;
-    std::cout<<"3. Square"<<std::endl;
-    std::cout<<"4. Rectangle"<<std::endl;
-    std::cout<<"5. Pentagon"<<std::endl;
-    std::cout<<"6. Hexagon"<<std::endl;
-    std::cout<<"7. Heptagon"<<std::endl;
-    std::cout<<"8. Octagon"<<std::endl;
-    std::cout<<"9. Nonagon"<<std::endl;
-    std::cout<<"10. Decagon"<<std::endl;
-    std::cout<<"0. Exit"<<std::endl;
+    print_menu();
 
     int selection; 
 
     std::cout<<"Enter Choice: ";
     std::cin>>selection; 
     
-    std::cout<<"Your Choice:"<< selection<<std::endl;
+    std::cout<<"Your Choice: "<< selection<<std::endl;
 }
