ba86e9f0f3604eed364f65cc587fcecaf060708d
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-17 11:37:46 +0500

    request input

--- short_programs/fortune.cpp
+++ short_programs/fortune.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string> 
+
+void requestInput(const std::string &prompt, std::string &input) {
+    std::cout << prompt; 
+    std::cin >> input;
+}
+
+void showMessage(const std::string &prompt) {
+    std::cout << prompt <<std::endl;
+}
 
 int main() {
-    std::cout<<"Welcome to the fortune teller program"<<std::endl;
+    showMessage("Welcome to the fortune teller program!");
 
+
+    std::string username;
+    requestInput("Please enter your name: ", username);
+
+    std::cout<<username<<std::endl;
     return 0;
 }
