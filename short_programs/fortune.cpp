ad2f98df91bd1f266942b0974ede99889c3be454
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-17 12:00:34 +0500

    request data

--- short_programs/fortune.cpp
+++ short_programs/fortune.cpp
@@ -3,20 +3,26 @@
 
 void requestInput(const std::string &prompt, std::string &input) {
     std::cout << prompt; 
-    std::cin >> input;
+    std::getline(std::cin, input);
 }
 
-void showMessage(const std::string &prompt) {
-    std::cout << prompt <<std::endl;
+void showMessage(const std::string &message) {
+    std::cout << message <<std::endl;
 }
 
 int main() {
     showMessage("Welcome to the fortune teller program!");
+    showMessage("");
 
 
-    std::string username;
-    requestInput("Please enter your name: ", username);
+    std::string username, season, adj1, adj2;
+    requestInput("Please enter your name: \n", username);
+    requestInput("Please enter the time of year when you were born:\n(pick from 'spring', 'summer', 'autumn', 'winter')\n", season);
+    requestInput("Please ennter an adjective: ", adj1);
+    requestInput("Please ennter another adjective: ", adj2);
 
-    std::cout<<username<<std::endl;
+
+
+    std::cout<<username<<" the "<<adj1<<" born in "<<season<<" is "<<adj2<<std::endl;
     return 0;
 }
