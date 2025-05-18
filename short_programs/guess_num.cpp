baa306c6fab091caca844929b018807e5b187960
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-18 10:08:01 +0500

    guess numbers

--- short_programs/guess_num.cpp
+++ short_programs/guess_num.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 
 void showMessage(const std::string &message) {
     std::cout << message <<std::endl;
@@ -15,7 +16,32 @@ int main() {
     std::cout<<"Please provide the largest number: "<<std::endl;
     std::cin>>largest;
 
+    std::random_device random_device;
+    std::mt19937 random_engine{random_device()};
+    std::uniform_real_distribution distribution{(float)smallest, (float)largest};
 
+    int generated_random_num = (int)distribution(random_engine);
+    showMessage("I've generated a number. Try to guess it!");
+
+    int guess;
+    bool guessed = false; 
+
+    int guesses = 0; 
+    while (!guessed) {
+        std::cout<<"Please provide the next guess: ";
+        std::cin>>guess;
+
+        if (guess < generated_random_num) {
+            showMessage("Your number is too small. Try again!");
+            guesses += 1;
+        } else if (guess > generated_random_num) {
+            showMessage("Your number is too big. Try again!");
+            guesses += 1;
+        } else if (guess == generated_random_num) {
+            std::cout<<"You've done it! You guessed the number "<<generated_random_num<<"  in "<<guesses<<" guesses!"<<std::endl;
+            guessed = true;
+        }
+    }
 
     return 0;
 }
