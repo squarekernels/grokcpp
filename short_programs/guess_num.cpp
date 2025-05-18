816ede169d8ef7259d923a8df082f6da497882ac
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-18 09:41:55 +0500

    get range inputs

--- short_programs/guess_num.cpp
+++ short_programs/guess_num.cpp
@@ -2,13 +2,20 @@
 
 void showMessage(const std::string &message) {
     std::cout << message <<std::endl;
-    std::cout << std::endl;
 }
+
 
 int main() {
 
-    showMessage("Welcome to the GUESSING GAME!\nI will generate a number and you will guess it!");
-    
-    
+    showMessage("Welcome to the GUESSING GAME!\nI will generate a number and you will guess it!");    
+    int smallest, largest; 
+    std::cout<<"Please provide the smallest number: "<<std::endl;
+    std::cin>>smallest;
+
+    std::cout<<"Please provide the largest number: "<<std::endl;
+    std::cin>>largest;
+
+
+
     return 0;
 }
